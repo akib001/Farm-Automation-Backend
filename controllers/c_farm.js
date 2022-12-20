834e6a7b67b370a9813943ed49d94477b63dc58f
Akib <akibahmed2011@gmail.com>
2022-12-20 23:25:10 +0600

    getAllFarmData Controller updated

--- controllers/c_farm.js
+++ controllers/c_farm.js
@@ -1,15 +1,13 @@
 const {validationResult} = require('express-validator/check');
-const mongoose = require('mongoose');
 const Farm = require('../models/m_farm');
 
 exports.getSingleFarmData = async (req, res, next) => {
     const farmerId = req.params.farmerId;
-    console.log("farmerId", farmerId)
     try {
         const farmData = await Farm.findOne({farmer_id: farmerId})
 
         if (!farmData) {
-            const error = new Error('No farm Data Found');
+            const error = new Error('Not a single farm Found');
             error.statusCode = 404;
             throw error;
         }
@@ -32,87 +30,17 @@ exports.getAllFarmData = async (req, res, next) => {
     try {
         const farmData = await Farm.find({farmer_id: farmerId})
 
-        if (!farmData) {
+
+        if (farmData?.length < 1) {
             const error = new Error('No farm Data Found');
             error.statusCode = 404;
             throw error;
         }
 
+        console.log('farmData', farmData)
         res.status(200).json({
             message: 'Fetched all farm data',
             data: farmData,
-        });
-    } catch (err) {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    }
-};
-
-
-exports.getTemp = async (req, res, next) => {
-    try {
-        const farmData = await Farm.findOne({})
-
-        console.log('farmData', farmData)
-
-        res.status(200).json({
-            message: 'Fetched latest temperature',
-            temp: farmData.temp,
-            updatedAt: farmData.updatedAt
-        });
-    } catch (err) {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    }
-};
-
-exports.getHumidity = async (req, res, next) => {
-    try {
-        const farmData = await Farm.findOne({})
-        res.status(200).json({
-            message: 'Fetched latest Humidity',
-            humidity: farmData.humidity,
-            updatedAt: farmData.updatedAt
-        });
-    } catch (err) {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    }
-};
-
-
-exports.getMoisture = async (req, res, next) => {
-    try {
-        const farmData = await Farm.findOne({})
-
-        res.status(200).json({
-            message: 'Fetched latest Moisture',
-            moisture: farmData.moisture,
-            updatedAt: farmData.updatedAt
-        });
-    } catch (err) {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    }
-};
-
-
-exports.getLight = async (req, res, next) => {
-    try {
-        const farmData = await Farm.findOne({})
-
-        res.status(200).json({
-            message: 'Fetched latest Light',
-            light: farmData.light,
-            updatedAt: farmData.updatedAt
         });
     } catch (err) {
         if (!err.statusCode) {
