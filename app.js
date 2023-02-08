6675a37e0c61def68383c726a3ab2e0a4fc65bf3
Akib <akibahmed2011@gmail.com>
2023-02-08 21:23:52 +0600

    starting code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require("cors");
+const pollRoutes = require('./routes/r_poll');
 const farmRoutes = require('./routes/r_farm');
 const farmerRoutes = require('./routes/r_farmer');
 const consumerRoutes = require('./routes/r_consumer');
@@ -11,20 +12,20 @@ const authRoutes = require('./routes/r_auth')
 const app = express();
 
 app.use(bodyParser.json()); // application/json
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: ['https://farm-automation.netlify.app', 'http://localhost:3000', 'http://localhost:3002'],
-        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+        origin: ['https://smartbd.vercel.app/', 'http://localhost:3000', 'http://localhost:3002'],
+        methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
         credentials: true,
     })
 );
-
-app.use('/farm', farmRoutes);
-app.use('/auth', authRoutes);
-app.use('/farmer', farmerRoutes);
-app.use('/consumer', consumerRoutes);
+app.use('/polls', pollRoutes);
+// app.use('/farm', farmRoutes);
+// app.use('/auth', authRoutes);
+// app.use('/farmer', farmerRoutes);
+// app.use('/consumer', consumerRoutes);
 
 
 app.use((error, req, res, next) => {
@@ -32,15 +33,15 @@ app.use((error, req, res, next) => {
     const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({message: message, data: data});
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose
     .connect(
-        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@node-tutorial.ps5lz.mongodb.net/farmAutomation?retryWrites=true`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        },
+        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@node-tutorial.ps5lz.mongodb.net/smartBdDb?retryWrites=true`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
     )
     .then(result => {
         app.listen(process.env.PORT || 8080);
